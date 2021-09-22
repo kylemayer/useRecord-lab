5151c5b43891f52dd44f9234c729f96bc1cd08b4
Kyle Mayer <x@kylemayer.dev>
2021-09-22 16:49:17 -0700

    added app js code from lab rubric, removed complexity and rendered current color to page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+const useRecord = (init) => {
+  const [current, setCurrent] = useState(init);
+  // const [colorArray, setColorArray] = useState(init);
+  // const [undo, setUndo] = useState('');
+  // const [redo, setRedo] = useState('');
+
+  return {
+    current,
+  };
+};
+
+function App() {
+  const { current } = useRecord('#FF0000');
+
+  return (
+    <>
+
+      <button onclick={undo}>undo</button>
+      <button>redo</button>
+      <input type="color" value={current} />
+      <div style={{ backgroundColor: current, width: '20px', height: '20px' }}></div>
+    </>
+  );
 }
+
+export default App;
